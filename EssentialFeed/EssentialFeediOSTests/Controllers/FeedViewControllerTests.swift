8e68bd324e079ddb2888e08c15838fe20d5439ff
wsaults <will@saults.io>
2022-03-07 10:57:37 -0600

    Extract FeedImageCell test helpers to a separate file

--- EssentialFeed/EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
@@ -335,33 +335,3 @@ class FeedViewControllerTests: XCTestCase {
         }
     }
 }
-
-private extension FeedImageCell {
-    func simulateRetryAction() {
-        feedImageRetryButton.simulateTap()
-    }
-
-    var isShowingLocation: Bool {
-        !locationContainer.isHidden
-    }
-
-    var isShowingImageLoadingIndicator: Bool {
-        feedImageContainer.isShimmering
-    }
-
-    var isShowingRetryAction: Bool {
-        !feedImageRetryButton.isHidden
-    }
-
-    var locationText: String? {
-        locationLabel.text
-    }
-
-    var descriptionText: String? {
-        descriptionLabel.text
-    }
-
-    var renderedImage: Data? {
-        feedImageView.image?.pngData()
-    }
-}
