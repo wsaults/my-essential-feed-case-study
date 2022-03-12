74bdc3632e570395d04f6ca196218faeaf8dabe8
wsaults <will@saults.io>
2022-03-12 14:52:50 -0600

    Move title string creation from `FeedViewController` to `FeedPresenter` - In MVP, presentation data should be created by the presenters

--- EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -25,6 +25,10 @@ final class FeedPresenter {
         self.loadingView = loadingView
     }
 
+    static var title: String {
+        "My Feed"
+    }
+
     func didStartLoadingFeed() {
         loadingView.display(FeedLoadingViewModel(isLoading: true))
     }
