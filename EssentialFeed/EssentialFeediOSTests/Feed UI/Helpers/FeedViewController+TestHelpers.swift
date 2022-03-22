231867c99273e912bab8e795033cb8f8d3717843
wsaults <will@saults.io>
2022-03-21 21:18:14 -0500

    Does not render load feed error on load

--- EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialFeed/EssentialFeediOSTests/Feed UI/Helpers/FeedViewController+TestHelpers.swift
@@ -43,6 +43,10 @@ extension FeedViewController {
         ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [index])
     }
 
+    var errorMessage: String? {
+        errorView.message
+    }
+
     func isShowingLoadingIndicator() -> Bool {
         refreshControl?.isRefreshing == true
     }
