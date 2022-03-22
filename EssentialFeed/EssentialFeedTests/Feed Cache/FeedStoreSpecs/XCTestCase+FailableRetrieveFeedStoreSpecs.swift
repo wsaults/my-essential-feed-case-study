83ad5517693a7bfbeb899a131277eea39712cea9
wsaults <will@saults.io>
2022-03-21 21:13:17 -0500

    Fix spacing in

--- EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FailableRetrieveFeedStoreSpecs.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FailableRetrieveFeedStoreSpecs.swift
@@ -10,10 +10,10 @@ import EssentialFeed
 
 extension FailableRetrieveFeedStoreSpecs where Self: XCTestCase {
     func assertThatRetrieveDeliversFailureOnRetrievalError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
-            expect(sut, toRetrieve: .failure(anyNSError()), file: file, line: line)
-        }
+        expect(sut, toRetrieve: .failure(anyNSError()), file: file, line: line)
+    }
 
-        func assertThatRetrieveHasNoSideEffectsOnFailure(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
-            expect(sut, toRetrieveTwice: .failure(anyNSError()), file: file, line: line)
-        }
+    func assertThatRetrieveHasNoSideEffectsOnFailure(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        expect(sut, toRetrieveTwice: .failure(anyNSError()), file: file, line: line)
+    }
 }
