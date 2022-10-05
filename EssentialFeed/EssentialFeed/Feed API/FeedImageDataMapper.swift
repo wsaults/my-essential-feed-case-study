f4088e412b3915ace50b46af688ac6d00b7ca56f
Will Saults <will@saults.io>
2022-10-05 10:20:44 -0500

    Test FeedImageDataMapper in isolation

---
+++ EssentialFeed/EssentialFeed/Feed API/FeedImageDataMapper.swift
@@ -0,0 +1,22 @@
+//
+//  FeedImageDataMapper.swift
+//  EssentialFeed
+//
+//  Created by Will Saults on 10/5/22.
+//
+
+import Foundation
+
+public final class FeedImageDataMapper {
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Data {
+        guard response.isOK, !data.isEmpty else {
+            throw Error.invalidData
+        }
+        
+        return data
+    }
+}
