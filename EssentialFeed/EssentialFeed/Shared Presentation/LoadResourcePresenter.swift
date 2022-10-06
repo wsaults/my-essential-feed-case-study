8842f91127a6db59c86325d8dece9be03c65801d
Will Saults <will@saults.io>
2022-10-06 09:32:23 -0500

    Duplicate FeedPresenter as LoadResourcePresenter

---
+++ EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -0,0 +1,49 @@
+//
+//  LoadResourcePresenter.swift
+//  EssentialFeed
+//
+//  Created by Will Saults on 10/6/22.
+//
+
+import Foundation
+
+public final class LoadResourcePresenter {
+    private var feedView: FeedView
+    private var loadingView: FeedLoadingView
+    private let errorView: FeedErrorView
+
+    public init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
+        self.feedView = feedView
+        self.loadingView = loadingView
+        self.errorView = errorView
+    }
+
+    public static var title: String {
+        NSLocalizedString("FEED_VIEW_TITLE",
+              tableName: "Feed",
+              bundle: Bundle(for: FeedPresenter.self),
+              comment: "Title for the feed view")
+    }
+
+    private var feedLoadError: String {
+        NSLocalizedString("FEED_VIEW_CONNECTION_ERROR",
+             tableName: "Feed",
+             bundle: Bundle(for: FeedPresenter.self),
+             comment: "Error message displayed when we can't load the image feed from the server")
+    }
+
+    public func didStartLoadingFeed() {
+        errorView.display(.noError)
+        loadingView.display(FeedLoadingViewModel(isLoading: true))
+    }
+
+    public func didFinishLoadingFeed(with feed: [FeedImage]) {
+        feedView.display(FeedViewModel(feed: feed))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+    }
+
+    public func didFinishLoadingFeed(with error: Error) {
+        errorView.display(.error(message: feedLoadError))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+    }
+}
