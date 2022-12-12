48c894cbffb83f71df0630ca4fc56031cfb3badd
Will Saults <will@saults.io>
2022-12-12 15:25:20 -0600

    Show comments on image selection

--- EssentialFeed/EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
+++ EssentialFeed/EssentialFeediOS/Shared UI/Controllers/ListViewController.swift
@@ -23,8 +23,6 @@ public final class ListViewController: UITableViewController, UITableViewDataSou
     public override func viewDidLoad() {
         super.viewDidLoad()
 
-        dataSource.defaultRowAnimation = .fade
-        tableView.dataSource = dataSource
         configureTableView()
         refresh()
     }
