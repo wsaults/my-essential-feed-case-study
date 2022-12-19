795f096c587c3d8cdf321a5d9a0944c2f2ddce7c
Will Saults <will@saults.io>
2022-12-19 08:44:39 -0600

    Make feed loader closure return Paginated<FeedImage>

--- EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+LoaderSpy.swift
+++ EssentialApp/EssentialAppTests/Helpers/FeedUIIntegrationTests+LoaderSpy.swift
@@ -1,5 +1,5 @@
 //
-//  FeedViewControllerTests+LoaderSpy.swift
+//  FeedUIIntegrationTests+LoaderSpy.swift
 //  EssentialFeediOSTests
 //
 //  Created by Will Saults on 3/7/22.
@@ -16,20 +16,20 @@ extension FeedUIIntegrationTests {
 
         // MARK: - FeedLoader
 
-        private var feedRequests = [PassthroughSubject<[FeedImage], Error>]()
+        private var feedRequests = [PassthroughSubject<Paginated<FeedImage>, Error>]()
 
         var loadFeedCallCount: Int {
             feedRequests.count
         }
         
-        func loadPublisher() -> AnyPublisher<[FeedImage], Error> {
-            let publisher = PassthroughSubject<[FeedImage], Error>()
+        func loadPublisher() -> AnyPublisher<Paginated<FeedImage>, Error> {
+            let publisher = PassthroughSubject<Paginated<FeedImage>, Error>()
             feedRequests.append(publisher)
             return publisher.eraseToAnyPublisher()
         }
 
         func completeFeedLoading(with feed: [FeedImage] = [], at index: Int = 0) {
-            feedRequests[index].send(feed)
+            feedRequests[index].send(Paginated(items: feed))
         }
 
         func completeFeedLoadingWithError(at index: Int = 0) {
---
+++ EssentialFeed/EssentialFeed/Shared API/Paginated.swift
@@ -0,0 +1,20 @@
+//
+//  Paginated.swift
+//  EssentialFeed
+//
+//  Created by Will Saults on 12/19/22.
+//
+
+import Foundation
+
+public struct Paginated<Item> {
+    public typealias LoadMoreCompletion = (Result<Self, Error>) -> Void
+    
+    public let items: [Item]
+    let loadMore: ((@escaping LoadMoreCompletion) -> Void)?
+    
+    public init(items: [Item], loadMore: ((@escaping LoadMoreCompletion) -> Void)? = nil) {
+        self.items = items
+        self.loadMore = loadMore
+    }
+}
