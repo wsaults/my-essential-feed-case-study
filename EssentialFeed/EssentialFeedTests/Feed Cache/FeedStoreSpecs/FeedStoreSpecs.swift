45dc060890cf22303063d9b67cc583a2d96fcc48
Will Saults <will@saults.io>
2022-12-22 20:58:54 -0600

    Make CoreData FeedStore implementation sync

--- EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs/FeedStoreSpecs.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/FeedStoreSpecs/FeedStoreSpecs.swift
@@ -21,8 +21,6 @@ protocol FeedStoreSpecs {
     func test_delete_hasNoSideEffectsOnEmptyCache()
     func test_delete_deliversNoErrorOnNonEmptyCache()
     func test_delete_emptiesPreviouslyInsertedCache()
-
-    func test_storeSideEffects_runsSerially()
 }
 
 protocol FailableRetrieveFeedStoreSpecs: FeedStoreSpecs {
