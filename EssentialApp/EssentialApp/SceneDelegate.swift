1c45087c63fc6d0bb510e0cfdd9ef39502529ff5
Will Saults <will@saults.io>
2022-12-22 21:16:25 -0600

    Implement subscribe(on:) for remote loader with fallback and remote load more loader

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -101,6 +101,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             .caching(to: localFeedLoader)
             .fallback(to: localFeedLoader.loadPublisher)
             .map(makeFirstPage)
+            .subscribe(on: scheduler)
             .eraseToAnyPublisher()
     }
     
@@ -110,6 +111,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             .map { (cachedItems, newItems) in (cachedItems + newItems, newItems.last) }
             .map(makePage)
             .caching(to: localFeedLoader)
+            .subscribe(on: scheduler)
+            .eraseToAnyPublisher()
     }
     
     private func makeRemoteFeedLoader(after: FeedImage? = nil) -> AnyPublisher<[FeedImage], Error> {
